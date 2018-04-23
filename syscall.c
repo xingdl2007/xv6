308206774dfbbc655ce7111f91628ea9ab01bf46
xingdl2007 <xingdl2007@gmail.com>
2018-04-23 15:42:06 +0800

    6.828 homework: xv6 system calls: part one: system call tracing

--- syscall.c
+++ syscall.c
@@ -104,6 +104,30 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 
+static const char* const syscall_names[]={
+  [SYS_fork]   "fork",
+  [SYS_exit]   "exit",
+  [SYS_wait]   "wait",
+  [SYS_pipe]   "pipe",
+  [SYS_read]   "read",
+  [SYS_kill]   "kill",
+  [SYS_exec]   "exec",
+  [SYS_fstat]  "fstat",
+  [SYS_chdir]  "chdir",
+  [SYS_dup]    "dup",
+  [SYS_getpid] "getpid",
+  [SYS_sbrk]   "sbrk",
+  [SYS_sleep]  "sleep",
+  [SYS_uptime] "uptime",
+  [SYS_open]   "open",
+  [SYS_write]  "write",
+  [SYS_mknod]  "mknod",
+  [SYS_unlink] "unlink",
+  [SYS_link]   "link",
+  [SYS_mkdir]  "mkdir",
+  [SYS_close]  "close",
+};
+
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
 [SYS_exit]    sys_exit,
@@ -141,5 +165,7 @@ syscall(void)
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
     curproc->tf->eax = -1;
-  }
+  } 
+  cprintf("%s -> %d\n", syscall_names[num],
+          curproc->tf->eax);
 }
