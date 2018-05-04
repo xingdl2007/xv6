b6d93132b1a0a034801ea9f00bec14be9465ea8c
xingdl2007 <xingdl2007@gmail.com>
2018-05-04 21:29:58 +0800

    6.828 homework: xv6 log

--- log.c
+++ log.c
@@ -66,17 +66,22 @@ initlog(int dev)
 }
 
 // Copy committed blocks from log to their home location
+// `warm` means the dirty block must be in buffer cache
+// so no need to copy from log
 static void
-install_trans(void)
+install_trans(int warm)
 {
   int tail;
 
   for (tail = 0; tail < log.lh.n; tail++) {
-    struct buf *lbuf = bread(log.dev, log.start+tail+1); // read log block
-    struct buf *dbuf = bread(log.dev, log.lh.block[tail]); // read dst
-    memmove(dbuf->data, lbuf->data, BSIZE);  // copy block to dst
+    struct buf *dbuf = bread(log.dev, log.lh.block[tail]);  // read dst
+    if(!warm) {
+      // read log block
+      struct buf *lbuf = bread(log.dev, log.start+tail+1);
+      memmove(dbuf->data, lbuf->data, BSIZE);    // copy block to dst
+      brelse(lbuf);
+    }
     bwrite(dbuf);  // write dst to disk
-    brelse(lbuf);
     brelse(dbuf);
   }
 }
@@ -116,7 +121,7 @@ static void
 recover_from_log(void)
 {
   read_head();
-  install_trans(); // if committed, copy from log to disk
+  install_trans(0); // if committed, copy from log to disk
   log.lh.n = 0;
   write_head(); // clear the log
 }
@@ -195,7 +200,7 @@ commit()
   if (log.lh.n > 0) {
     write_log();     // Write modified blocks from cache to log
     write_head();    // Write header to disk -- the real commit
-    install_trans(); // Now install writes to home locations
+    install_trans(1); // Now install writes to home locations
     log.lh.n = 0;
     write_head();    // Erase the transaction from the log
   }
